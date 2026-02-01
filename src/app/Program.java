6298b4abd4627bac4d3c25abb98badc71e085c19
Theisson <theissondias194@gmail.com>
2026-02-01 07:30:06 -0300

    Database seed

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,9 +1,16 @@
 package app;
 
 import database.SchemaManager;
+import database.Database;
 
 public class Program {
     public static void main(String[] args) {
-        SchemaManager.createTables();
+        try {
+            SchemaManager.createTables();
+            SchemaManager.seedData();
+        }
+        finally {
+            Database.closeConnection();
+        }
     }
 }
